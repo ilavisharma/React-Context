f16dfdffd2cd66fdb89133740ed019bfb8febdc7
ilavisharma <lavisharma796@gmail.com>
2019-03-17 19:34:04 +0530

    add color to state

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,10 @@ import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
-  state = { language: "english" };
+  state = { language: "english", color: "primary" };
 
-  onLanguageChange = language => {
-    this.setState({ language });
+  onLanguageChange = (language, color) => {
+    this.setState({ language, color });
   };
 
   render() {
@@ -18,15 +18,15 @@ class App extends React.Component {
           Select a language:
           <i
             className="flag us"
-            onClick={() => this.onLanguageChange("english")}
+            onClick={() => this.onLanguageChange("english", "primary")}
           />
           <i
             className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
+            onClick={() => this.onLanguageChange("dutch", "red")}
           />
         </div>
         {/* order doesn't matter for these context providers */}
-        <ColorContext.Provider value="red">
+        <ColorContext.Provider value={this.state.color}>
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
           </LanguageContext.Provider>
