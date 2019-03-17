da7dd69c7fb96c7deafac7885bce4d956e7a662d
ilavisharma <lavisharma796@gmail.com>
2019-03-17 19:34:36 +0530

    add helper func for color context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,28 @@
 import React from "react";
 
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   renderSubmit = value => {
     return value === "english" ? "Submit" : "Voorleggen";
   };
 
-  render() {
+  renderColor = color => {
     return (
-      <button className="ui primary button">
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {value => this.renderSubmit(value)}
         </LanguageContext.Consumer>
       </button>
+    );
+  };
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderColor(color)}
+      </ColorContext.Consumer>
     );
   }
 }
